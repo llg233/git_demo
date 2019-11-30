7b4a80b55ea35e6a8c92a723da8314e55718d973
llg <2218455958@qq.com>
2019-11-30 16:39:13 +0800

    修改了User.java,提交到本地仓库 创建Customer.java

---
+++ git_demo/src/main/java/com/llg/domain/Customer.java
@@ -0,0 +1,13 @@
+package com.llg.domain;
+
+/**
+ * @Author: liulonggen
+ * @Date: 2019/11/30 16:37
+ * @Package: com.llg.domain
+ * @Description:
+ */
+public class Customer {
+    public static void main(String[] args) {
+
+    }
+}
