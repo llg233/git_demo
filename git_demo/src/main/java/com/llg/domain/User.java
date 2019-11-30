b5029561c77cbc09b4eaf446cc64f92f57385f51
llg <2218455958@qq.com>
2019-11-30 11:29:58 +0800

    新建User.java,提交到本地仓库

---
+++ git_demo/src/main/java/com/llg/domain/User.java
@@ -0,0 +1,12 @@
+package com.llg.domain;
+
+/**
+ * @Author: liulonggen
+ * @Date: 2019/11/30 11:23
+ * @Package: com.llg.domain
+ * @Description:
+ */
+public class User {
+    private Integer id;
+    private String username;
+}
